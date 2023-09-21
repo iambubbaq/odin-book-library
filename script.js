ddb5fcf47a7723d24df410689f36bc9301638d6c
Stephen Williams <25148210+iambubbaq@users.noreply.github.com>
2023-09-21 13:07:30 -0600

    Add form submission to add book to library

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 i = 0;
-let newBook;
 let library = document.getElementById("library");
-let bookFormButton = document.getElementById("bookFormButton")
+let bookFormButton = document.getElementById("bookFormButton");
+let addBookButton = document.getElementById("addBookButton");
 bookFormButton.addEventListener("click",openBookForm);
+addBookButton.addEventListener("click",addBookToLibrary);
 
 const myLibrary = [];
 
@@ -13,17 +14,26 @@ function Book(Title, Author, Pages, Status) {
     this.Status = Status;
 };
 
-function addBookToLibrary() {
-    newBook = new Book();
-    myLibrary.push("newBook");
-};
-
 function bookToShelf() {
     while (i < myLibrary.length) {
         library.innerHTML += `<div class="book"><h2>${myLibrary[i].Title}</h2><p>Author: ${myLibrary[i].Author}</p><p>Pages: ${myLibrary[i].Pages}</p><p>Status: ${myLibrary[i].Status}</p>`;
         i++;
     };
 }
+
+function addBookToLibrary(Title, Author, Pages, Status) {
+    Title = document.getElementById("Title").value;
+    Author = document.getElementById("Author").value;
+    Pages = document.getElementById("Pages").value;
+    Status = document.getElementById("Status").value;
+    let newBook = new Book(Title, Author, Pages, Status);
+    myLibrary.push(newBook);
+    document.getElementById("addPopUp").style.display = "none";
+    document.getElementById("bookForm").reset();
+    bookToShelf();
+
+    
+};
 
 function openBookForm() {
     document.getElementById("addPopUp").style.display = "block";
