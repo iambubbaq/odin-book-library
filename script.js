79a3710efd0d1de75fd737dea2797c3ab3ea13a5
Stephen Williams <25148210+iambubbaq@users.noreply.github.com>
2023-09-21 11:44:13 -0600

    Add new book form

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 i = 0;
+let newBook;
 let library = document.getElementById("library");
+let bookFormButton = document.getElementById("bookFormButton")
+bookFormButton.addEventListener("click",openBookForm);
 
-const myLibrary = [{"Title": "A Game of Thrones", "Author": "G.R.R. Martin", "Pages": "600", "Status": "Read"}, {"Title": "Beauty and the Beast", "Author": "Disney", "Pages": "2000", "Status": "Not Read"}];
+const myLibrary = [];
 
-function Book() {
+function Book(Title, Author, Pages, Status) {
+    this.Title = Title;
+    this.Author = Author;
+    this.Pages = Pages;
+    this.Status = Status;
 };
 
 function addBookToLibrary() {
+    newBook = new Book();
+    myLibrary.push("newBook");
 };
 
 function bookToShelf() {
@@ -14,5 +23,9 @@ function bookToShelf() {
         library.innerHTML += `<div class="book"><h2>${myLibrary[i].Title}</h2><p>Author: ${myLibrary[i].Author}</p><p>Pages: ${myLibrary[i].Pages}</p><p>Status: ${myLibrary[i].Status}</p>`;
         i++;
     };
+}
+
+function openBookForm() {
+    document.getElementById("addPopUp").style.display = "block";
 }
 
